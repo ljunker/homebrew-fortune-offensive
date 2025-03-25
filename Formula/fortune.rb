a2cffc3b35a6ab5b9aac98ffe3dd5165df5ffb81
Lars Junker <lj@hbt.de>
2025-03-25 11:30:47 +0100

    correct hash

--- Formula/fortune.rb
+++ Formula/fortune.rb
@@ -2,7 +2,7 @@ class Fortune < Formula
   desc "Infamous UNIX fortune program"
   homepage "http://bxr.su/OpenBSD/games/fortune/"
   url "https://github.com/shlomif/fortune-mod/archive/refs/tags/fortune-mod-3.24.0.tar.gz"
-  sha256 "5b70d3ec5d5d56e6a7c9decd452f2e27a1be91cfda4e8bdb9b32798709c1e881"
+  sha256 "0cb889a124994377190800b2d70cf2254791049c8ba55d7570f91936d38c85ad"
   license "BSD-4-Clause"
 
   def install
