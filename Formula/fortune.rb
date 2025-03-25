d3b021f000f5403159cb886542562b85fbe7e3c9
Lars Junker <lj@hbt.de>
2025-03-25 11:19:04 +0100

    init commit

---
+++ Formula/fortune.rb
@@ -0,0 +1,18 @@
+
+class Fortune < Formula
+  desc "Infamous UNIX fortune program"
+  homepage "https://www.gnu.org/software/fortune/"
+  url "https://github.com/shlomif/fortune-mod/archive/refs/tags/fortune-mod-3.24.0.tar.gz"
+  sha256 "5b70d3ec5d5d56e6a7c9decd452f2e27a1be91cfda4e8bdb9b32798709c1e881"
+  license "BSD-4-Clause"
+
+  def install
+    system "make", "OFFENSIVE=1"
+    bin.install "fortune"
+    share.install "datfiles"
+  end
+
+  test do
+    system "#{bin}/fortune"
+  end
+end
